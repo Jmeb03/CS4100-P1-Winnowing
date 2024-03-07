8dfcfa3490162f14a5129834ef509b8a4d3d32f8
Jmeb03 <jb908320@ohio.edu>
2024-03-07 18:58:53 -0500

    workinggit add .

--- Makefile
+++ Makefile
@@ -1,16 +1,17 @@
-all: main
+all: scanner cmos run_script
 
-CXX = clang++
-override CXXFLAGS += -g -Wmost -Werror
+scanner: lex.yy.c
+	g++ -o scanner lex.yy.c -lfl
 
-SRCS = $(shell find . -name '.ccls-cache' -type d -prune -o -type f -name '*.cpp' -print | sed -e 's/ /\\ /g')
-HEADERS = $(shell find . -name '.ccls-cache' -type d -prune -o -type f -name '*.h' -print)
+cmos: cmos.cpp
+	g++ -o cmos cmos.cpp
 
-main: $(SRCS) $(HEADERS)
-	$(CXX) $(CXXFLAGS) $(SRCS) -o "$@"
+lex.yy.c: cmos.l
+	flex cmos.l
 
-main-debug: $(SRCS) $(HEADERS)
-	$(CXX) $(CXXFLAGS) -U_FORTIFY_SOURCE -O0 $(SRCS) -o "$@"
+run_script:
+	sudo bash detector.sh Examples/Examples
 
+.PHONY: clean
 clean:
-	rm -f main main-debug
+	rm -f scanner cmos lex.yy.c
--- cmos.cpp
+++ cmos.cpp
@@ -10,49 +10,108 @@
 #include <cstdlib>
 #include <vector>
 #include <fstream>
+#include <set>
+#include <algorithm>
 using namespace std;
 
-///function prototypes
+size_t KGRAMSIZE = 11;
+size_t WINDOWSIZE = 21;
+/// function prototypes
 
-
-vector<string> generateKGrams(const string& text, int k) {
-    vector<string> kGrams;
-
-    for (int i = 0; i <= text.length() - k; ++i) {
-        string kGram = text.substr(i, k);
-        kGrams.push_back(kGram);
-    }
-
-    return kGrams;
+/**
+ * @brief Takes the intersection of two k-gram sets
+ * 
+ * @param hashedkGrams The vector of k-gram sets
+ * @param i The index of the first k-gram set
+ * @param j The index of the second k-gram set
+ * @return The intersection of the two k-gram sets
+ */
+set<int> intersectionKGrams(vector<set<int>> hashedkGrams, int i, int j)
+{
+  set<int> intersectionSet;
+  set_intersection(hashedkGrams[i].begin(), hashedkGrams[i].end(), hashedkGrams[j].begin(), hashedkGrams[j].end(), inserter(intersectionSet, intersectionSet.begin()));
+  return intersectionSet;
 }
 
+/** 
+* @brief GENERATES OUR KGRAMS AND DOES THE FINGERPRINT ANALYSIS
+* @param text the string of tokens
+* @param k the size of each kgram
+* @return a set of hashed kgrams
+*/
+set<int> generateKGrams(const string &text, int k)
+{
+  vector<int> kGrams;
 
+  for (size_t i = 0; i <= text.length() - k; ++i) //text.length() - k for bounding the kgrams
+  {
 
-int main(int argc, char const *argv[]) {
+    string kGram = text.substr(i, k);
+
+    // hash the kgram
+    int hashedGram = 0;
+    for (size_t j = 0; j < kGram.length(); ++j)
+    {
+      hashedGram += kGram[j];
+      hashedGram *= (j + 1);
+    }
+    hashedGram %= 1007; //seemed like a fun number
+
+    kGrams.push_back(hashedGram);
+  }
+  // FINGERPRINT + WINDOW ANAYLSIS
+  set<int> fingerprintSet;
+  int smallest;
+  for (size_t j = 0; j <= kGrams.size() - WINDOWSIZE; j++) //
+  {
+    smallest = kGrams[j];
+    for (size_t i = 0; i < WINDOWSIZE; i++)
+    {
+      if (kGrams[i + j] < smallest)
+      {
+        smallest = kGrams[i + j];
+      }
+    }
+    fingerprintSet.insert(smallest);
+  }
+
+  return fingerprintSet;
+}
+
+int main(int argc, char const *argv[])
+{
 
   ifstream inFile;
   inFile.open("tokens.txt");
-  if(inFile.fail()){
+  if (inFile.fail())
+  {
     cout << "File failed to open" << endl;
     return 0;
   }
 
-  string pretendstring = "ABCDEFGABCDEFGABCDEFG";
+  vector<set<int>> hashedkGrams;
+  for (int i = 0; i < 54; i++)
+  {
+    string temp;
+    getline(inFile, temp);
+    hashedkGrams.push_back(generateKGrams(temp, KGRAMSIZE));
+  }
 
-  
-  int k = 54; //size of our k-gram
-  vector <string> kGrams;
-  for(int i = 0; i < k; i++)
+  set<int> intersection;
+  for (int a = 0; a < 54; a++) // amount of files
+  {
+
+    for (int b = 0; b < 54; b++) //matches them with amount of files
     {
-      string temp;
-      inFile.ignore(' ');
-      getline(inFile, temp);
-      cout << temp << endl << endl;
+      set<int> intersection = intersectionKGrams(hashedkGrams, a, b);
+      float percentShared = (float)intersection.size() / (float)hashedkGrams[a].size() * 100.0f;
+      if (percentShared > 75.0f && b != a) //ONLY KEEP TOP 75% OF MATCHES --- MOST LIKELY TO BE PLARAGARISM
+      {
+        cout << "File " << a + 1 << " is " << percentShared << "% similar to File " << b + 1 << endl;
+      }
     }
+  }
 
-  
-
-
-  
-   return 0;
-}// main
+  inFile.close();
+  return 0;
+} // main
